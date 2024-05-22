0fa7fec28448720cbdbd2436c562f432e83fa5f8
Mouli Bhaskar <duddupudimouli@gmail.com>
2024-05-22 19:22:34 +0530

    'modified to ORM'

--- app/models.py
+++ app/models.py
@@ -11,4 +11,5 @@ class Post(Base):
     title = Column(String, nullable = False)
     content = Column(String, nullable = False)
     published = Column(Boolean, server_default = 'TRUE', nullable = False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable = False, server_default = text('now()'))
+    created_at = Column(TIMESTAMP(timezone=True),
+                         nullable = False, server_default = text('now()'))
